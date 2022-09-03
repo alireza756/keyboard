6da767a1839afd7a43fb3a7cfc946b258b8c45a1
alireza756 <alirezacheraghliei992@gmail.com>
2022-09-04 02:08:01 +0430

    updated help section

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let btn = document.querySelector('body');
 let value = document.querySelectorAll('button');
+let text = document.querySelector('textarea');
 btn.addEventListener('keydown' , function(e){
-    for(let i = 0 ; i <= 34 ; i++){
+    for(let i = 0 ; i <= 33 ; i++){
         if(value[i].innerHTML == e.key.toUpperCase()){
             value[i].classList.toggle('active');
+            text.value += value[i].innerHTML;
         }
     }}
 )
 btn.addEventListener('keyup' , function(e){
-    for(let i = 0 ; i <= 34 ; i++){
+    for(let i = 0 ; i <= 33 ; i++){
         if(value[i].innerHTML == e.key.toUpperCase()){
             value[i].classList.toggle('active');
         }
