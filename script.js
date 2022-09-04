a5bdcf31ad29bc6db355762ed258ba86c828033c
alireza756 <alirezacheraghliei992@gmail.com>
2022-09-04 13:35:42 +0430

    Added RGB

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let btn = document.querySelector('body');
 let value = document.querySelectorAll('button');
 let text = document.querySelector('textarea');
+let helpe_btn = document.querySelector('.help-button');
+helpe_btn.addEventListener('click' , function(){
+  document.querySelector('.help-box').style.top = "100px";
+  document.querySelector('.help-box').style.opacity=  "1";
+})
 btn.addEventListener('keydown' , function(e){
     for(let i = 0 ; i <= 33 ; i++){
         if(value[i].innerHTML == e.key.toUpperCase()){
             value[i].classList.toggle('active');
-            text.value += value[i].innerHTML;
+            let a = Math.floor(Math.random() * 250);
+            let b = Math.floor(Math.random() * 250);
+            let c = Math.floor(Math.random() * 250);
+            value[i].style.color = `rgb(${a}, ${b}, ${c})`;
+            text.value += value[i].innerHTML.toLowerCase();
         }
     }}
 )
@@ -13,6 +22,7 @@ btn.addEventListener('keyup' , function(e){
     for(let i = 0 ; i <= 33 ; i++){
         if(value[i].innerHTML == e.key.toUpperCase()){
             value[i].classList.toggle('active');
+            value[i].style.color = "black";
         }
     }
 })
