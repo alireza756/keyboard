82256a3914426fb389b3dcd2593967f681a57dda
alireza756 <alirezacheraghliei992@gmail.com>
2022-09-05 15:54:16 +0430

    add a new feature

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ let btn = document.querySelector('body');
 let value = document.querySelectorAll('button');
 let text = document.querySelector('textarea');
 let helpe_btn = document.querySelector('.help-button');
+//About us button
 helpe_btn.addEventListener('click' , function(){
   document.querySelector('.help-box').style.top = "100px";
   document.querySelector('.help-box').style.opacity=  "1";
 })
 btn.addEventListener('keydown' , function(e){
+    //new feature for show About us button
+    if(e.altKey && e.key == 'a'){
+        document.querySelector('.help-box').style.top = "100px";
+        document.querySelector('.help-box').style.opacity=  "1";
+    }
+    //find button and add style for client
     for(let i = 0 ; i <= 33 ; i++){
         if(value[i].innerHTML == e.key.toUpperCase()){
             value[i].classList.toggle('active');
@@ -18,6 +25,7 @@ btn.addEventListener('keydown' , function(e){
         }
     }}
 )
+//find button and remove style from selected button
 btn.addEventListener('keyup' , function(e){
     for(let i = 0 ; i <= 33 ; i++){
         if(value[i].innerHTML == e.key.toUpperCase()){
